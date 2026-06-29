af4cedbf034fc240ff45cbfb76e582e83e7b8b18
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2026-06-29 17:02:12 +0530

    Fix for some packages not updating on the first run

--- multipass.fish
+++ multipass.fish
@@ -4,11 +4,15 @@
 # multipass LTS server named - rr
 set lts rr
 
-set ver 1.2
-
-#TODO: Display the time to execute the functions.
+set ver 1.3
 
 # changelog
+# 1.3
+#   - date: 2026-06-27
+#   - double upgrade pass for fresh clones
+#   - better DEBIAN_FRONTEND handling inside multipass exec
+#   - keep real warnings visible
+#   - improve logging / output.
 # 1.2
 #   - date: 2026-06-08
 #   - fix formatting.
@@ -21,24 +25,21 @@ set ver 1.2
 #   - add a log file
 
 set --local PATH ~/.local/bin ~/bin /usr/local/bin /usr/bin /sbin /bin
-set -x DEBIAN_FRONTEND noninteractive
 set time_start
-set time_end
-set run_time
 
 function manage-multipass --description 'Manage multipass servers'
-    argparse 'c/create' 'd/delete' 'u/update' -- $argv
+    argparse 'c/create' 'd/delete' 'u/update=' -- $argv
     or return
 
     if test (count $argv_opts) -eq 0
-        echo Usage: manage-m.fish -c/--create -d/--delete
+        echo "Usage: manage-multipass.fish -c/--create -d/--delete -u/--update="
         return 0
     end
 
     if set -ql _flag_create
         __bootstrap
         __delete_test
-        __update_lts
+        __update_server $lts
         __create_test
         __cleanup
         osascript -e 'display notification "Bootstrap completed!" with title "Multipass manager"' 2>/dev/null
@@ -52,82 +53,109 @@ function manage-multipass --description 'Manage multipass servers'
         return 0
     end
 
-    if set -q _flag_update
+    if set -ql _flag_update
         __bootstrap
-        __update_lts
+        # echo Update flag is set.
+        # echo "Update flag value: '$_flag_update'"
+        __update_server $_flag_update
         __cleanup
         return 0
     end
-
 end
 
 function __bootstrap
-    echo Script started on "$(date +%c)"
+    echo "Script started on $(date +%c)"
     echo
     set time_start (date +%s)
 end
 
 function __cleanup
     echo
-    set time_end (date +%s)
-    set run_time (math $time_end - $time_start)
-    echo Execution time: $run_time seconds
-    echo Script ended on "$(date +%c)"
+    set -l time_end (date +%s)
+    set -l run_time (math $time_end - $time_start)
+    echo "Execution time: $run_time seconds"
+    echo "Script ended on $(date +%c)"
     echo
 end
 
 function __create_test
-    echo Current servers list...
+    echo "Current servers list..."
     multipass list
 
     multipass list | grep -qw '^test'
     if test $status -ne 0
         multipass clone -n test $lts
-        echo A test server is created from Resolute Raccoon.
+        echo "A test server is created from Resolute Raccoon."
         multipass start test
-        echo Test server has been started.
+        echo "Test server has been started."
         multipass set client.primary-name=test
-        echo Test server is made as primary.
+        echo "Test server is made as primary."
     else
-        echo The test server exists.
+        echo "The test server does not exist." >&2
     end
     echo
 end
 
 function __delete_test
     multipass set client.primary-name=$lts
-    echo Resolute Raccoon is made as primary server.
+    echo "Resolute Raccoon is made as primary server."
 
     multipass list | grep -qw '^test'
     if test $status -eq 0
-        echo Hold on while deleting the test server...
+        echo "Hold on while deleting the test server..."
         multipass delete test
-        echo Test server is deleted.
+        echo "Test server is deleted."
 
         multipass purge
-        echo Purged unused resources.
+        echo "Purged unused resources."
     else
-        echo Test server does not exist.
+        echo "Test server does not exist."
     end
 
-    echo Current servers list...
+    echo "Current servers list..."
     multipass list
     echo
 end
 
-function __update_lts
-    multipass set client.primary-name=$lts
-    echo Set Resolute Raccoon as primary server.
-    echo 'Starting the (primary) server...'
-    multipass start
-    echo Refreshing apt cache...
-    multipass exec $lts -- sudo apt-get update -qq
-    echo 'Updating packages (if any)...'
-    multipass exec $lts -- sudo apt-get upgrade -y -qq
-    echo 'Removing packages (if any)...'
-    multipass exec $lts -- sudo apt-get autoremove -y -qq
-    multipass stop
-    echo Stopped Resolute Raccoon server.
+function __update_server --description "Updates server packages"
+    if test (count $argv) -eq 0
+        echo "Function Usage: __update_server <name>"
+        return 1
+    end
+
+    set --local _server $argv[1]
+    if test -z $_server
+        set _server rr
+    end
+
+    if test $_server = '='
+        set _server rr
+    end
+
+    multipass list | grep -qw "^$_server"
+    if test $status -ne 0
+        echo "The supplied server '$_server' is not found. So, it can not be updated." >&2
+        exit
+    end
+
+    echo 'Starting the server Resolute Raccoon ...'
+    multipass start $_server
+
+    printf '\t%s\n' "Refreshing apt cache..."
+    multipass exec $_server -- sudo apt-get update -qq
+
+    printf '\t%s\n' 'Updating packages (if any)...'
+    multipass exec $_server -- sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y -qq
+
+    # Second pass catches packages that only become available after first update
+    printf '\t%s\n' 'Running second upgrade pass (important for fresh clones)...'
+    multipass exec $_server -- sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y -qq
+
+    printf '\t%s\n' 'Removing unnecessary packages...'
+    multipass exec $_server -- sudo DEBIAN_FRONTEND=noninteractive apt-get autoremove -y -qq
+
+    multipass stop $_server
+    echo "Stopped the server Resolute Raccoon"
     echo
 end
 
